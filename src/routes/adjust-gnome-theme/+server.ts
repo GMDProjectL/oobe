30b063f11b1f849692456bf65ad235250e009f0e
Andrew <61388196+relativemodder@users.noreply.github.com>
2025-07-03 04:02:57 +0300

    Update gnome font

--- src/routes/adjust-gnome-theme/+server.ts
+++ src/routes/adjust-gnome-theme/+server.ts
@@ -5,8 +5,8 @@ import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
     const childProcess = spawn('gsettings', ['set', 'org.gnome.desktop.interface', 'gtk-theme', 'adw-gtk3-dark' ]);
-    const childProcess1 = spawn('gsettings', ['set', 'org.gnome.desktop.interface', 'monospace-font-name', 'SF Mono Semi-Bold 10' ]);
-    const childProcess2 = spawn('gsettings', ['set', 'org.gnome.desktop.interface', 'font-name', 'SF Compact Display Light 10' ]);
+    const childProcess1 = spawn('gsettings', ['set', 'org.gnome.desktop.interface', 'monospace-font-name', 'Adwaita Mono 10' ]);
+    const childProcess2 = spawn('gsettings', ['set', 'org.gnome.desktop.interface', 'font-name', 'Adwaita Sans 10' ]);
     const childProcess3 = spawn('gsettings', ['set', 'org.gnome.desktop.interface', 'color-scheme', 'prefer-dark' ]);
     
     return new Response();
